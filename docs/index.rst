4041a2ee9c08270f8d1d0c4bcf750817de40ca0c
arbennett <bennett.andr@gmail.com>
2018-05-03 10:27:43 -0700

    Make skeleton

---
+++ docs/index.rst
@@ -0,0 +1,9 @@
+.. hyeenna documentation master file
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+.. _index:
+
+HYEENNA: Hydrologic Entropy Estimators based on Nearest Neighbor Approximations
+==================================
+
+This is a placeholder
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(name='hyeenna',
+      version='0.0.1',
+      description='Hydrologic Entropy Estimators based on Nearest Neighbor Approximations',
+      url='https://github.com/arbennett/HYEENNA',
+      author='Andrew Bennett',
+      author_email='bennett.andr@gmail.com',
+      packages=['hyeenna'],
+      install_requires=['xarray', 'scikit-learn', 'numpy', 'pandas'],
+      keywords=['hydrology', 'climate', 'information theory', 'statistics'],
+      tests_require=['pytest'],)
